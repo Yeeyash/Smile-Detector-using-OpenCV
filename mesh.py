439f4ee90c797d68a12b15905a92141bd451156d
yeeYash <thakareyash99258@gmail.com>
2025-04-01 21:47:40 +0530

    Connections are not displayed, Fixing soon

--- mesh.py
+++ mesh.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture(0)
 # this time we wont be plotting connections since there are too many points to consider.
 mpdraw = mp.solutions.drawing_utils
 mpfacemesh = mp.solutions.face_mesh
-facemesh = mpfacemesh.FaceMesh() #This is needed to create objects.
+facemesh = mpfacemesh.FaceMesh(static_image_mode=False) #This is needed to create objects.
 
 # FaceMesh(), takes 4 parameters, static_image_mode(False,default), max_num_faces, min_detection_confidence, min_tracking_confidence.
 
@@ -14,6 +14,12 @@ while True:
     success, img = cap.read()
     flippedimg = cv2.flip(img, 1)
     imgrgb = cv2.cvtColor(flippedimg, cv2.COLOR_BGR2RGB)
+
+    results = facemesh.process(imgrgb)
+
+    if results.multi_face_landmarks:
+        for facelms in results.multi_face_landmarks: #lms - landmarks
+            mpdraw.draw_landmarks(img, facelms, mpfacemesh.FACEMESH_CONTOURS) #broke here.
 
     cv2.imshow("image", flippedimg)
     cv2.waitKey(1)

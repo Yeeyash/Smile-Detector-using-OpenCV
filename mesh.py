214ba86c40833d9fdd7dbb52eaa157bf7622a6db
yeeYash <thakareyash99258@gmail.com>
2025-04-02 19:01:48 +0530

    Finished face meshing, smile detection remains

--- mesh.py
+++ mesh.py
@@ -7,6 +7,8 @@ cap = cv2.VideoCapture(0)
 mpdraw = mp.solutions.drawing_utils
 mpfacemesh = mp.solutions.face_mesh
 facemesh = mpfacemesh.FaceMesh(static_image_mode=False) #This is needed to create objects.
+drawspec = mpdraw.DrawingSpec(color = (0,255,0),thickness=1, circle_radius=2)
+
 
 # FaceMesh(), takes 4 parameters, static_image_mode(False,default), max_num_faces, min_detection_confidence, min_tracking_confidence.
 
@@ -19,7 +21,7 @@ while True:
 
     if results.multi_face_landmarks:
         for facelms in results.multi_face_landmarks: #lms - landmarks
-            mpdraw.draw_landmarks(flippedimg, facelms, mpfacemesh.FACEMESH_CONTOURS) #broke here.
+            mpdraw.draw_landmarks(flippedimg, facelms, mpfacemesh.FACEMESH_CONTOURS, drawspec) #broke here.
 
     cv2.imshow("image", flippedimg)
     cv2.waitKey(1)

2e8b05f960da3a993745cb0a7329db03df8eddb3
yeeYash <thakareyash99258@gmail.com>
2025-04-03 17:47:06 +0530

    Smile detections works, Not very Accurate.
    Only works when teeth are not visible or smile is wider than taller.

--- mesh.py
+++ mesh.py
@@ -9,6 +9,7 @@ mpfacemesh = mp.solutions.face_mesh
 facemesh = mpfacemesh.FaceMesh(static_image_mode=False) #This is needed to create objects.
 drawspec = mpdraw.DrawingSpec(color = (0,255,0),thickness=1, circle_radius=2)
 
+lipindices = list(range(61,68)) + list(range(78,95))
 
 # FaceMesh(), takes 4 parameters, static_image_mode(False,default), max_num_faces, min_detection_confidence, min_tracking_confidence.
 
@@ -21,7 +22,25 @@ while True:
 
     if results.multi_face_landmarks:
         for facelms in results.multi_face_landmarks: #lms - landmarks
-            mpdraw.draw_landmarks(flippedimg, facelms, mpfacemesh.FACEMESH_CONTOURS, drawspec) #broke here.
+            # mpdraw.draw_landmarks(flippedimg, facelms, mpfacemesh.FACEMESH_CONTOURS, drawspec) #broke here.
+
+            left_mouth_corner = facelms.landmark[61]
+            right_mouth_corner = facelms.landmark[291]
+            upper_lip_top = facelms.landmark[0]
+            lower_lip_bottom = facelms.landmark[17]
+
+            mouth_width = (abs(right_mouth_corner.x - left_mouth_corner.x))
+            mouth_height = (abs(upper_lip_top.y - lower_lip_bottom.y))
+            
+            mouth_ratio = mouth_width/mouth_height if mouth_height != 0 else 0
+
+            if mouth_ratio >2.5:
+                cv2.putText(img, "smili", (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 1)
+                print(mouth_ratio)
+                print("smil")
+
+            mpdraw.draw_landmarks(flippedimg, facelms, mpfacemesh.FACEMESH_CONTOURS, drawspec)
+
 
     cv2.imshow("image", flippedimg)
     cv2.waitKey(1)

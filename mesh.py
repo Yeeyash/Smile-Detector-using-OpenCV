bce0a70634cb9de10116fd81a2b1ea9d1869ae88
yeeYash <thakareyash99258@gmail.com>
2025-04-05 15:43:01 +0530

    Lip detection using FaceMesh Module completed.

--- mesh.py
+++ mesh.py
@@ -2,18 +2,6 @@ import cv2
 import mediapipe as mp
 import time
 
-currentlysimling = False
-previouslysmiling = False
-
-lasttime = 0
-debouncetime = 1000
-smiled = 0
-
-upperlistindices = [61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 291]
-lowerlipindices = [146, 91, 181, 84, 17, 314, 405, 321, 375, 291]
-innerupperlistindices = [78, 191, 80, 81, 82, 13, 312, 311, 310, 415, 308]
-innerlowerlipindices = [78, 95, 88, 178, 87, 14, 317, 402, 318, 317, 14]
-
 class Smile_Detector():
     def __init__(self, staticMode=False, maxFaces=2, DetectionCon=0.5, TrackCon=0.5):
         
@@ -27,77 +15,85 @@ class Smile_Detector():
         self.mpfacemesh = mp.solutions.face_mesh
         self.facemesh = self.mpfacemesh.FaceMesh(static_image_mode = self.staticMode, max_num_faces = self.maxFaces, min_detection_confidence = self.DetectionCon, min_tracking_confidence = self.TrackCon)
         self.drawspec = self.mpdraw.DrawingSpec(color = (0,255,0),thickness=1, circle_radius=2)
+        
+        self.currentlysimling = False
+        self.previouslysmiling = False
+
+        self.lasttime = 0
+        self.debouncetime = 1000
+        self.smiled = 0
+
+        self.upperlistindices = [61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 291]
+        self.lowerlipindices = [146, 91, 181, 84, 17, 314, 405, 321, 375, 291]
+        self.innerupperlistindices = [78, 191, 80, 81, 82, 13, 312, 311, 310, 415, 308]
+        self.innerlowerlipindices = [78, 95, 88, 178, 87, 14, 317, 402, 318, 317, 14]
 
     def lipemesh(self, img, draw= True):
         self.imgrgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.facemesh.process(self.imgrgb)
+        ih, iw, _ = img.shape
+        liplms = {}
 
         if self.results.multi_face_landmarks:
             for facelms in self.results.multi_face_landmarks: #lms - landmarks
-                self.mpdraw.draw_landmarks(img, facelms, self.mpfacemesh.FACEMESH_CONTOURS, self.drawspec, self.drawspec) #broke here.
 
-                # ih, iw, _ = self.img.shape
+                alllipindx = self.upperlistindices + self.lowerlipindices + self.innerlowerlipindices + self.innerupperlistindices
+                for idx in alllipindx:
+                    landmark = facelms.landmark[idx]
+                    x, y = int(landmark.x * iw), int(landmark.y * ih)
 
-                # liplms = {}
-                # alllipindx = upperlistindices + lowerlipindices + innerlowerlipindices + innerupperlistindices
-                # for idx in alllipindx:
-                #     landmark = facelms.landmark[idx]
-                #     x, y = int(landmark.x * iw), int(landmark.y * ih)
-
-                #     liplms[idx] = (x,y)
-                #     cv2.circle(img, (x,y), 2, (0,255,0), -1)
+                    liplms[idx] = (x,y)
+                    cv2.circle(img, (x,y), 2, (0,255,0), -1)
             
-        return img
-   
-# FaceMesh(), takes 4 parameters, static_image_mode(False,default), max_num_faces, min_detection_confidence, min_tracking_confidence.
-
-# def issmiling(liplandmark):
-#     left_mouth_corner = facelms.landmark[61]
-#     right_mouth_corner = facelms.landmark[291]
-#     upper_lip_top = facelms.landmark[0]
-#     lower_lip_bottom = facelms.landmark[17]
-
-#     mouth_width = (abs(right_mouth_corner.x - left_mouth_corner.x))
-#     mouth_height = (abs(upper_lip_top.y - lower_lip_bottom.y))
-            
-#     mouth_ratio = mouth_width/mouth_height if mouth_height != 0 else 0
-
-#     return mouth_ratio >2.5 and mouth_height*100 > 3.5
-
-    # if issmiling(liplms) != previouslysmiling:
-    #                 currenttime = time.time() * 1000 #time in miliseconds.
-    #                 print("smliked", currenttime, lasttime)
-    #                 if (currenttime - lasttime) > debouncetime:
-    #                     print(smiled)
-    #                     smiled = smiled + 1
-    #                     lasttime = currenttime
-
-    #             previouslysmiling = issmiling(liplms)
-#     
-
-            # printing smiled(smliked) whenever detected, error in timing.
-
-            # mpdraw.draw_landmarks(flippedimg, facelms, mpfacemesh.FACEMESH_CONTOURS, drawspec)
+                return img, facelms
 
 
+        return img, None
     
+    def issmiling(self, facelms):
+        left_mouth_corner = facelms.landmark[61]
+        right_mouth_corner = facelms.landmark[291]
+        upper_lip_top = facelms.landmark[0]
+        lower_lip_bottom = facelms.landmark[17]
 
-def main():
-    cap = cv2.VideoCapture(0)
-    detector = Smile_Detector()
+        mouth_width = (abs(right_mouth_corner.x - left_mouth_corner.x))
+        mouth_height = (abs(upper_lip_top.y - lower_lip_bottom.y))
+                
+        mouth_ratio = mouth_width/mouth_height if mouth_height != 0 else 0
 
-    while True:
-        success, img = cap.read()
-        flippedimg = cv2.flip(img, 1)
+        return mouth_ratio > 2.5 and mouth_height*100 > 3.5
 
-        img = detector.lipemesh(img, True)
 
-        cv2.imshow("image", img)
-        cv2.waitKey(1)
+    def main(self):
+        cap = cv2.VideoCapture(0)
+        detector = Smile_Detector()
+
+        while True:
+            success, img = cap.read()
+            flippedimg = cv2.flip(img, 1)
+
+            flippedimg, facelms = self.lipemesh(flippedimg, True)
+            
+            if facelms:
+                currentlysmiling = self.issmiling(facelms)
+                
+                if currentlysmiling != self.previouslysmiling:
+                    currenttime = time.time() * 1000
+                    if (currenttime - self.lasttime) > self.debouncetime:
+                        self.smiled = self.smiled + 1
+                        print(self.smiled, currenttime, self.lasttime)
+                        self.lasttime = currenttime
+                self.previouslysmiling = currentlysmiling
+
+
+            cv2.imshow("image", flippedimg)
+            cv2.waitKey(1)
 
 
 if __name__ == "__main__":
-    main()
+    detector = Smile_Detector()
+    detector.main()
+
 
 
 # trying to show video with mesh firstly.
